e6e9a4a21146e4de5840b221148a210a42acca5b
Keita Urashima <ursm@ursm.jp>
2026-01-12 14:58:28 +0900

    Fix bug where feed body gets double-escaped

--- spec/requests/feeds_controller_spec.rb
+++ spec/requests/feeds_controller_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe FeedsController, type: :request do
   example 'GET /feed' do
-    create :post
+    create :post, body: '# hello world'
 
     get '/feed.atom'
 
     expect(response).to have_http_status(:ok)
     expect(response.content_type).to include('application/atom+xml')
+
+    content  = Nokogiri::XML.parse(response.body).at_css('content')
+    fragment = Nokogiri::HTML::DocumentFragment.parse(content.text)
+
+    expect(fragment.at_css('h1').text).to eq('hello world')
   end
 end
